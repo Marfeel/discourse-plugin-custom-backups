c5f47258960e59eec89c8f68f6cd2f84a25dd07e
Andres Tarrio <143702884+andrestarriomrf@users.noreply.github.com>
2025-06-18 16:30:33 -0300

    Sys 110547/add api endpoint (#3)
    * chore: add API endpoint (SYS-110547)
    
    * chore: add API endpoint (SYS-110547)
    
    * chore: add API endpoint (SYS-110547)
    
    * chore: add API endpoint (SYS-110547)
    
    * chore: add API endpoint (SYS-110547)
    
    * chore: add API endpoint (SYS-110547)
    
    * chore: add API docs (SYS-110547)
    
    * chore: update seetings (SYS-110547)
    
    * chore: run in background (SYS-110547)

--- lib/controllers/backup_controller.rb
+++ lib/controllers/backup_controller.rb
@@ -7,13 +7,8 @@ module BackupApi
 
       def create
         script_path = Rails.root.join("script", "run_local_backup.rb").to_s
-        result = `bundle exec ruby #{script_path} 2>&1`
-
-        if $?.success?
-          render json: { success: true, output: result }
-        else
-          render json: { success: false, error: result }, status: 500
-        end
+        Process.spawn("bundle exec ruby #{script_path} > log/local_backup.log 2>&1")
+        render json: { success: true, message: "Backup running in background." }
       end
     end
-  end
+end
