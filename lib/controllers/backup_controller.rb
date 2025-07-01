59ac1738c4c3369a78dfd17f538f8715de2a74c2
andrestarriomrf <andres.tarrio@marfeel.com>
2025-07-01 13:05:36 -0300

    chore: add backup endpoints (SYS-110547)

--- lib/controllers/backup_controller.rb
+++ lib/controllers/backup_controller.rb
@@ -10,5 +10,35 @@ module BackupApi
         Process.spawn("bundle exec ruby #{script_path} > log/local_backup.log 2>&1")
         render json: { success: true, message: "Backup running in background." }
       end
+
+      def index
+        bucket = SiteSetting.discourse_plugin_custom_backups_s3_bucket.presence || 'my-test-bucket'
+
+        s3 = Aws::S3::Client.new(
+          region:            SiteSetting.discourse_plugin_custom_backups_s3_region.presence     || 'us-east-1',
+          access_key_id:     SiteSetting.discourse_plugin_custom_backups_s3_access_key.presence || 'test',
+          secret_access_key: SiteSetting.discourse_plugin_custom_backups_s3_secret_key.presence || 'test',
+          endpoint:          SiteSetting.discourse_plugin_custom_backups_s3_endpoint.presence   || 'http://localhost:4566',
+          force_path_style:  true
+        )
+
+        begin
+          objects = s3.list_objects_v2(bucket: bucket).contents
+
+          backup_list = objects.map do |obj|
+            {
+              key: obj.key,
+              last_modified: obj.last_modified,
+              size: obj.size,
+              url: "#{SiteSetting.discourse_plugin_custom_backups_s3_endpoint}/#{bucket}/#{obj.key}"
+            }
+          end
+
+          render json: backup_list
+        rescue Aws::S3::Errors::ServiceError => e
+          render json: { error: "Error al obtener los backups: #{e.message}" }, status: 500
+        end
+      end
+
     end
 end
--- lib/routes/discourse_plugin_custom_backups_routes.rb
+++ lib/routes/discourse_plugin_custom_backups_routes.rb
@@ -1,3 +1,4 @@
 Discourse::Application.routes.append do
     post '/discourse-plugin-custom-backups/run' => 'backup_api/backups#create'
+    get  '/discourse-plugin-custom-backups/list' => 'backup_api/backups#index'
 end
